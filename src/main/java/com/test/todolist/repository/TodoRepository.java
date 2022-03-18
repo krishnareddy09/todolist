6f434825af14356dbe48bed23e8ad0952ca659a4
krishnareddy09 <chkrishnareddy09@gmail.com>
2022-03-18 15:04:18 +0800

    initial commit

---
+++ src/main/java/com/test/todolist/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.test.todolist.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.test.todolist.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{
+	List<Todo> findByUserName(String user);
+}
